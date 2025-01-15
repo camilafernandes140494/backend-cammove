9e69c4dbce77147edf6ed850e10fc2bb38983fad
Camila <camilaferna140494@gmail.com>
2025-01-15 09:49:36 -0300

    fix: add port for vercel

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Usando a variável de ambiente PORT (definido pelo Vercel ou outro servidor)
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
