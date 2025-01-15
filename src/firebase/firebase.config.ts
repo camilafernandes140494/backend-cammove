8cd3bd9bdfe63d15b667b636c4bf0b12d45bdbb4
Camila <camilaferna140494@gmail.com>
2025-01-15 11:06:44 -0300

    fix: firebase config

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,10 +1,12 @@
 // src/firebase/firebase.config.ts
 import * as admin from 'firebase-admin';
-import * as path from 'path';
+import { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount = require(
-  path.resolve(__dirname, '../../config/serviceAccountKey.json'),
-);
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.PROJECT_ID!,
+  clientEmail: process.env.CLIENT_EMAIL!,
+  privateKey: process.env.PRIVATE_KEY!.replace(/\\n/g, '\n'),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
