1f313acf2e9391b8a505af5ffe70ba686b2f2666
Camila <camilaferna140494@gmail.com>
2025-02-04 21:59:13 -0300

    fix: test exercise

--- src/exercises/exercises.service.ts
+++ src/exercises/exercises.service.ts
@@ -1,71 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { Exercise, UpdateExercise } from './exercises.types';
+import { Exercise } from './exercises.types';
 import admin from 'src/firebase/firebase.config';
 
 @Injectable()
 export class ExercisesService {
   private firestore = admin.firestore();
+  private collection = 'exercises'; // Cada exercício será um documento nesta coleção
 
-  async createExercise(exerciseData: Exercise): Promise<any> {
+  async createExercise(exercise: Exercise): Promise<any> {
     try {
-      // Cria um novo documento na coleção 'exercises' com dados de 'exerciseData'
-      const newExerciseRef = this.firestore.collection('exercises').doc();
+      const newExerciseRef = this.firestore.collection(this.collection).doc();
 
-      // Armazena o objeto diretamente como o conteúdo do documento
-      await newExerciseRef.set(exerciseData);
+      await newExerciseRef.set({
+        ...exercise,
+        id: newExerciseRef.id, // Armazena o ID do documento
+        createdAt: new Date().toISOString(),
+        updatedAt: '',
+        deletedAt: '',
+      });
 
-      // Retorna uma mensagem com o ID do documento criado
       return { message: 'Exercício criado com sucesso', id: newExerciseRef.id };
     } catch (error) {
-      // Se ocorrer um erro, lança uma exceção com a mensagem de erro
       throw new Error('Erro ao salvar o exercício: ' + error.message);
     }
   }
 
-  async getExercises(filters: UpdateExercise): Promise<Exercise[]> {
-    let query: FirebaseFirestore.Query<FirebaseFirestore.DocumentData> =
-      this.firestore.collection('exercises');
+  async getExercises(filters: Partial<Exercise>): Promise<Exercise[]> {
+    let query: FirebaseFirestore.Query = this.firestore.collection(
+      this.collection,
+    );
 
-    // Aplica os filtros fornecidos
-    if (filters.name) {
-      query = query
-        .orderBy('name')
-        .startAt(filters.name)
-        .endAt(filters.name + '\uf8ff');
-    }
-    if (filters.description) {
-      query = query
-        .orderBy('description')
-        .startAt(filters.description)
-        .endAt(filters.description + '\uf8ff');
-    }
-    if (filters.deletedAt) {
-      query = query.where('deletedAt', '==', filters.deletedAt);
-    }
     if (filters.category) {
-      query = query
-        .orderBy('category')
-        .startAt(filters.category)
-        .endAt(filters.category + '\uf8ff');
+      query = query.where('category', '==', filters.category);
     }
     if (filters.muscleGroup) {
-      query = query
-        .orderBy('muscleGroup')
-        .startAt(filters.muscleGroup)
-        .endAt(filters.muscleGroup + '\uf8ff');
+      query = query.where('muscleGroup', '==', filters.muscleGroup);
     }
 
     const snapshot = await query.get();
-    if (snapshot.empty) {
-      return [];
-    }
-
-    const exercises: Exercise[] = [];
-    snapshot.forEach((doc) => {
-      const data = doc.data() as Exercise;
-      exercises.push(data);
-    });
-
-    return exercises;
+    return snapshot.docs.map((doc) => doc.data() as Exercise);
   }
 }
